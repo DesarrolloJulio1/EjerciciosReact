fe414b16d2f2c1c17cdd8a6da0717f9b5fa56fdd
julio <desarrollofrontendweb@cormago.com>
2022-09-15 15:44:55 -0500

    agregando nuevo proyecto

---
+++ Js-React/index.js
@@ -0,0 +1,13 @@
+const user = {
+    name: 'julio',
+    lastname: 'morales',
+    age: 25,
+}
+
+function printInfo(user) {
+    return '<h1> hola ' + user.name + ' ' + user.age + '</h1'
+}
+
+console.log(printInfo(user))
+
+document.body.innerHTML = printInfo(user)
