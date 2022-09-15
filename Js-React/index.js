ff408df0e37a83d906ca4ee5030e7a2dc5489721
julio <desarrollofrontendweb@cormago.com>
2022-09-15 16:35:07 -0500

    modificacion js prueba

--- Js-React/index.js
+++ Js-React/index.js
@@ -4,10 +4,13 @@ const user = {
     age: 25,
 }
 
+
 function printInfo(user) {
     return '<h1> hola ' + user.name + ' ' + user.age + '</h1'
 }
 
 console.log(printInfo(user))
+
+//document.body.append(button)
 
 document.body.innerHTML = printInfo(user)
